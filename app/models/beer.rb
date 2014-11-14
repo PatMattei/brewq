5f06166eeb0480eadd980b60c2dbb8381b6cb1d1
Patrick Mattei <patrick.mattei@gmail.com>
2014-11-13 21:58:03 -0500

    Adding new beers to db

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -13,6 +13,8 @@ class Beer < ActiveRecord::Base
     json_file = RestClient.get("http://api.brewerydb.com/v2/beers/?format=json&key=#{ENV['BEER_DB_API_KEY']}&withBreweries=Y&abv=#{abv}&srm=#{color}&ibu=#{taste}")  
     JSON.parse(json_file)  
   end
+
+  def self.check_db_for_beer(api_id)
+    Beer.where(:api_id => "#{api_id}").blank?
+  end
 end
-
-
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@ class Review < ActiveRecord::Base
   belongs_to :beer
 
   belongs_to :user
+
+
+  def self.check_for_beer
+  end
 end
