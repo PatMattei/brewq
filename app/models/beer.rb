771666dfa5092472d78670896b3c9b9cd77c1714
Patrick Mattei <patrick.mattei@gmail.com>
2014-12-02 22:47:48 -0500

    Found beers cleaned up; add_beer_to_db working

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,7 +2,7 @@ class Beer < ActiveRecord::Base
   has_many :users, :through => :reviews
 
   has_many :reviews
-accepts_nested_attributes_for :reviews
+  accepts_nested_attributes_for :reviews
 
   def self.get_from_api(abv, taste, color)
     json_file = RestClient.get("http://api.brewerydb.com/v2/beers/?format=json&key=#{ENV['BEER_DB_API_KEY']}&withBreweries=Y&abv=#{abv}&srm=#{color}&ibu=#{taste}")  
@@ -13,6 +13,10 @@ accepts_nested_attributes_for :reviews
     Beer.where(:api_id => "#{api_id}").blank?
   end
 
+  def self.add_beer_to_db(beer_name, beer_brewery, beer_id, beer_style, beer_abv, beer_srm, beer_ibu, beer_description, beer_labels)
+    Beer.create name:beer_name, brewery:beer_brewery, api_id:beer_id,  style:beer_style, abv:beer_abv, srm:beer_srm, ibu:beer_ibu, description:beer_description, label:beer_labels
+  end
+  
   def self.get_reviews
     if Review.where(:beer_id).blank?
       "Write the first review"
